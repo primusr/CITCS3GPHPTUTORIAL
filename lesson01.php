dd8968b98c24eb537ea2f34661182842a8436c36
unknown <primusr@S312.UC-BCF.EDU.PH>
2023-09-25 18:38:11 +0800

    Lesson01

---
+++ lesson01.php
@@ -0,0 +1,12 @@
+<?php
+    echo "hello world";
+    echo "blah blahe blah";
+    echo "<br> blah eblah blah x2";
+    echo "<br> blah belah blah x3";
+    echo "<br> blahe blah blah x4";
+
+    echo "<br> blah blah blah x2";
+    echo "jsflkjsdlkf";
+    
+
+?>
---
+++ lesson02.php
@@ -0,0 +1,3 @@
+<?php 
+    echo "php lesson 2";
+?>
