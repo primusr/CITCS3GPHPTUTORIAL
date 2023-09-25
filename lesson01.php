b0ac2dde3affcca6f811032f4c680054159d693a
unknown <primusr@S312.UC-BCF.EDU.PH>
2023-09-25 18:48:15 +0800

    Basic Syntax

--- lesson01.php
+++ lesson01.php
@@ -1,12 +1,39 @@
-<?php
-    echo "hello world";
-    echo "blah blahe blah";
-    echo "<br> blah eblah blah x2";
-    echo "<br> blah belah blah x3";
-    echo "<br> blahe blah blah x4";
-
-    echo "<br> blah blah blah x2";
-    echo "jsflkjsdlkf";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic Syntax</title>
+</head>
+<body>
     
+   
 
-?>
+    <?php 
+        #this is a single line 
+        echo "this echo comes after a single line comment";
+        /* 
+         
+        pisicing elit. Exercitationem, 
+        necessitatibus quam porro at vero culpa 
+        asperiores quis, quia doloremque repellat ducimus. 
+        Cumque laborum repellat ipsa laboriosam quo quas non! Magnam.
+        
+        */
+
+        echo "this echo comes after a multiline comment";
+
+        
+
+
+    
+    
+    
+    
+    
+    
+    ?>
+
+
+</body>
+</html>
